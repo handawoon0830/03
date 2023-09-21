5f0077372ea5f52c636c32ec1a5d9f9c61d54335
handawoon0830 <handawoon0830@naver.com>
2023-09-21 16:33:47 +0900

    4

--- main.c
+++ main.c
@@ -4,11 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	char c;
+	int a;
+	int b;
+	float c;
 	
-	printf("enter the character: ");
-	scanf("%c",&c);
+	printf(" enter tne first number: ");
+	scanf("%d",&a);
 	
-	printf("the next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);	
+	printf(" enter the second number: ");
+	scanf("%d",&b);
+	
+	c= (float)a/b;
+	
+	printf("the divided answer of %d and %d is %f", a, b, c);
 	return 0;
 }
